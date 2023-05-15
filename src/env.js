c55d9c9725dd864c6c938dc65158cfbe354bb45c
estiven2111 <estiven.a.martinez@gmail.com>
2023-05-15 17:32:38 -0500

    prueba

--- src/env.js
+++ src/env.js
@@ -1,10 +1,10 @@
-URL_FRONT = "urbanclub.club"
-URL_BACK =  "https://pruebaback-production-0050.up.railway.app"
+// URL_FRONT = "urbanclub.club"
+// URL_BACK =  "https://pruebaback-production-0050.up.railway.app"
 
 // URL_FRONT = "urbanclub.club"
 // URL_BACK =  "https://pruebaback-production-0050.up.railway.app"
 
-// URL_FRONT = "http://localhost:3000"
-// URL_BACK =  "http://localhost:3001"
+URL_FRONT = "http://localhost:3000"
+URL_BACK =  "http://localhost:3001"
 
 module.exports = {URL_FRONT,URL_BACK}
